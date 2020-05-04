d6b19c1b2b77e40a98764fe31850908cde3ebacc
GeassCoder <limiaousa@gmail.com>
2020-05-04 10:22:16 -0400

    use lowercase component names

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './app.vue'
+import app from './app.vue'
 import router from './router'
 import store from './store'
 
@@ -8,7 +8,12 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
+  render: function (h) { return h(app) }
 }).$mount('#app')
 
 // TODO: try normal way of creating vue instance
+// new Vue({
+//   el: '#app',
+//   router,
+//   store
+// });
