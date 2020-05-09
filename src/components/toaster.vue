925507edf2c5fa75669afe5f2358407eccda0bbe
GeassCoder <limiaousa@gmail.com>
2020-05-09 19:30:53 -0400

    checkpoint commit, make it run

--- src/components/toaster.vue
+++ src/components/toaster.vue
@@ -10,7 +10,7 @@ export default {
   name: 'toaster',
   methods: {
     close () {
-      $store.dispatch('updateToasterMessage', '')
+      this.$store.dispatch('updateToasterMessage', '')
     }
   }
 }
